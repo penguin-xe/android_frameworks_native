18cee69d2610ef3493d77ba2b5e86ccd685c7cd8
Arne Coucheron <arco68@gmail.com>
2020-11-06 15:22:40 -0500

    SurfaceFlinger: Allow disabling resource cleanup from previous frame
    Causes lag on some legacy devices.
    
    Change-Id: I89913d214c7377c73bd307696dbf9aac2f9a5c0a
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3087,12 +3087,16 @@ void SurfaceFlinger::postComposition(PhysicalDisplayId pacesetterId,
     /* QTI_END */
 
     if (hasPacesetterDisplay && !pacesetterDisplay->isPoweredOn()) {
+#ifndef DISABLE_POSTRENDER_CLEANUP
         getRenderEngine().cleanupPostRender();
+#endif
         return;
     }
 
+#ifndef DISABLE_POSTRENDER_CLEANUP
     // Cleanup any outstanding resources due to rendering a prior frame.
     getRenderEngine().cleanupPostRender();
+#endif
 
     {
         std::lock_guard lock(mTexturePoolMutex);
